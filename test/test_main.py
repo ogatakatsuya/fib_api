63225b8539558491c8aa6f14ae81539c229e619b
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-05-21 22:27:03 +0900

    add tests

--- test/test_main.py
+++ test/test_main.py
@@ -8,7 +8,19 @@ from app import app
 import pytest
 import json
 
-def test_index_route():
+def test_1():
+    response = app.test_client().get('/fib?n=1')
+    data = json.loads(response.data)
+    assert response.status_code == 200
+    assert data['result'] == 1
+    
+def test_2():
+    response = app.test_client().get('/fib?n=2')
+    data = json.loads(response.data)
+    assert response.status_code == 200
+    assert data['result'] == 1
+
+def test_5():
     response = app.test_client().get('/fib?n=5')
     data = json.loads(response.data)
     assert response.status_code == 200
